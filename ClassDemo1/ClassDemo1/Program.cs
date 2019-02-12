791d8eb3127c008383e1d6debb949f8ca425ef32
plinck <paul.linck@gmail.com>
2019-02-12 16:24:46 -0500

    Got connection string from app.config

--- ClassDemo1/ClassDemo1/Program.cs
+++ ClassDemo1/ClassDemo1/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.Common;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace ClassDemo1
 {
@@ -15,6 +15,26 @@ namespace ClassDemo1
                 Person paul = new Person("Paul");
 
                 Console.WriteLine($"Hello {paul.Name}");
+
+                var connStr =
+                    System.Configuration.ConfigurationManager.
+                        ConnectionStrings["bamazon"].ConnectionString;
+                var conn = new MySqlConnection(connStr);
+
+                if (conn.State != ConnectionState.Open)
+                    conn.Open();
+
+                var cmd = new MySqlCommand(@"SELECT * FROM products;", conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                    
+                DataTable products = new DataTable();
+                products.Load(reader);
+
+                foreach (DataRow row in products.Rows)
+                {
+                    Console.WriteLine(row["product_name"].ToString());
+                }
+
                 Console.ReadLine();
             }
 
