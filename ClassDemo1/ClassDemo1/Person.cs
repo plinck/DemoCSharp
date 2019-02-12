4afdf421b202f8f3796fd0305e7da6ced7ffa155
plinck <paul.linck@gmail.com>
2019-02-12 15:18:06 -0500

    Added person class and string interpolation

---
+++ ClassDemo1/ClassDemo1/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace ClassDemo1
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ ClassDemo1/ClassDemo1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Person paul = new Person("Paul");
+
+                Console.WriteLine($"Hello {paul.Name}");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
